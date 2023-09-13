cc7afecc8b3303d84b71c2db87812d669a2ea51e
marcin.pierchala@icloud.com <marcin.pierchala@icloud.com>
2023-09-13 06:52:03 +0200

    Add project files.

---
+++ PhotosForSale/Data/ApplicationDbConext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhotosForSale.Models;
+
+namespace PhotosForSale.Data
+{
+    public class ApplicationDbConext : DbContext
+    {
+        public ApplicationDbConext(DbContextOptions<ApplicationDbConext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 8, CategoryName = "Krajobraz", DisplayOrder = 1 },
+                new Category { Id = 9, CategoryName = "Portret", DisplayOrder = 2 },
+                new Category { Id = 10, CategoryName = "Natura", DisplayOrder = 3 }
+                );
+        }
+    }
+}
+}
---
+++ PhotosForSale/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace PhotosForSale.Models
+{
+    public class Category
+    {
+        [Key]
+
+        public int? Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        [DisplayName("Category Name")]
+        public string CategoryName { get; set; }
+
+        [DisplayName("Display Order")]
+        [Range(1, 20)]
+        public int DisplayOrder { get; set; }
+    }
+}
