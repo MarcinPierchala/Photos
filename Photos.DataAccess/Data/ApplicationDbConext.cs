f6cc50d66b8af44328fb790cb0f8d2f1fdd77e12
marcin.pierchala@icloud.com <marcin.pierchala@icloud.com>
2023-09-19 18:40:45 +0200

    addFileUrlFieldAndSetMultipartFormData

--- Photos.DataAccess/Data/ApplicationDbConext.cs
+++ Photos.DataAccess/Data/ApplicationDbConext.cs
@@ -21,67 +21,67 @@ namespace Photos.DataAccess.Data
             //    new Category { Id = 10, CategoryName = "Natura", DisplayOrder = 3 }
             //    );
 
-            modelBuilder.Entity<MyPhoto>().HasData(
-                new MyPhoto
-                {
-                    Id = 1,
-                    Title = "Fortune of Time",
-                    Author = "Billy Spark",
-                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
-                    Price = 90,
-                    CategoryId = 13,
-                    ImageUrl = ""
-                },
-                new MyPhoto
-                {
-                    Id = 2,
-                    Title = "Dark Skies",
-                    Author = "Nancy Hoover",
-                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
-                    Price = 30,
-                    CategoryId = 13,
-                    ImageUrl = ""
-                },
-                new MyPhoto
-                {
-                    Id = 3,
-                    Title = "Vanish in the Sunset",
-                    Author = "Julian Button",
-                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
-                    Price = 50,
-                    CategoryId = 13,
-                    ImageUrl = ""
-                },
-                new MyPhoto
-                {
-                    Id = 4,
-                    Title = "Cotton Candy",
-                    Author = "Abby Muscles",
-                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
-                    Price = 65,
-                    CategoryId = 13,
-                    ImageUrl = ""
-                },
-                new MyPhoto
-                {
-                    Id = 5,
-                    Title = "Rock in the Ocean",
-                    Author = "Ron Parker",
-                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
-                    Price = 27,
-                    CategoryId = 13,
-                    ImageUrl = ""
-                },
-                new MyPhoto
-                {
-                    Id = 6,
-                    Title = "Leaves and Wonders",
-                    Author = "Laura Phantom",
-                    Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
-                    Price = 23,
-                    CategoryId = 13,
-                    ImageUrl = ""
-                });
+            //modelBuilder.Entity<MyPhoto>().HasData(
+            //    new MyPhoto
+            //    {
+            //        Id = 1,
+            //        Title = "Fortune of Time",
+            //        Author = "Billy Spark",
+            //        Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+            //        Price = 90,
+            //        CategoryId = 13,
+            //        ImageUrl = ""
+            //    },
+            //    new MyPhoto
+            //    {
+            //        Id = 2,
+            //        Title = "Dark Skies",
+            //        Author = "Nancy Hoover",
+            //        Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+            //        Price = 30,
+            //        CategoryId = 13,
+            //        ImageUrl = ""
+            //    },
+            //    new MyPhoto
+            //    {
+            //        Id = 3,
+            //        Title = "Vanish in the Sunset",
+            //        Author = "Julian Button",
+            //        Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+            //        Price = 50,
+            //        CategoryId = 13,
+            //        ImageUrl = ""
+            //    },
+            //    new MyPhoto
+            //    {
+            //        Id = 4,
+            //        Title = "Cotton Candy",
+            //        Author = "Abby Muscles",
+            //        Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+            //        Price = 65,
+            //        CategoryId = 13,
+            //        ImageUrl = ""
+            //    },
+            //    new MyPhoto
+            //    {
+            //        Id = 5,
+            //        Title = "Rock in the Ocean",
+            //        Author = "Ron Parker",
+            //        Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+            //        Price = 27,
+            //        CategoryId = 13,
+            //        ImageUrl = ""
+            //    },
+            //    new MyPhoto
+            //    {
+            //        Id = 6,
+            //        Title = "Leaves and Wonders",
+            //        Author = "Laura Phantom",
+            //        Description = "Praesent vitae sodales libero. Praesent molestie orci augue, vitae euismod velit sollicitudin ac. Praesent vestibulum facilisis nibh ut ultricies.\r\n\r\nNunc malesuada viverra ipsum sit amet tincidunt. ",
+            //        Price = 23,
+            //        CategoryId = 13,
+            //        ImageUrl = ""
+            //    });
         }
     }
 }
---
+++ Photos.DataAccess/Migrations/20230919154639_addedMigrationaafterAddMigration.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Photos.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedMigrationaafterAddMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
+                    DisplayOrder = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MyPhotos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MyPhotos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MyPhotos_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MyPhotos_CategoryId",
+                table: "MyPhotos",
+                column: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MyPhotos");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
