8839263d46979cd1978e9f98aec0df2899b7a7db
MarcinPierchala <macintecmarcin@gmail.com>
2023-09-21 10:16:00 +0200

    AddedIdentityDbContext

--- Photos.DataAccess/Data/ApplicationDbConext.cs
+++ Photos.DataAccess/Data/ApplicationDbConext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Photos.Models.Models;
 
 namespace Photos.DataAccess.Data
 {
-    public class ApplicationDbConext : DbContext
+    public class ApplicationDbConext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbConext(DbContextOptions<ApplicationDbConext> options) : base(options)
         {
@@ -14,7 +16,7 @@ namespace Photos.DataAccess.Data
         public DbSet<MyPhoto> MyPhotos { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //base.OnModelCreating(modelBuilder);
+            base.OnModelCreating(modelBuilder);
             //modelBuilder.Entity<Category>().HasData(
             //    new Category { Id = 8, CategoryName = "Krajobraz", DisplayOrder = 1 },
             //    new Category { Id = 9, CategoryName = "Portret", DisplayOrder = 2 },
--- PhotosForSale/Program.cs
+++ PhotosForSale/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using Photos.DataAccess.Data;
 using Photos.DataAccess.Repository;
 using Photos.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbConext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbConext>();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
