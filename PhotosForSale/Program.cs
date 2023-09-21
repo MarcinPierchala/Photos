e182569440d668bca3ad902178553d2ef0952a39
MarcinPierchala <macintecmarcin@gmail.com>
2023-09-21 10:27:54 +0200

    addPartialViewRegisterLoginSomeCosmetics

--- PhotosForSale/Program.cs
+++ PhotosForSale/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbConext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbConext>();
+builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbConext>();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
@@ -30,6 +30,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
