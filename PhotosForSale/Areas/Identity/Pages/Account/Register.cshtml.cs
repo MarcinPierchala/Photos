817d786722ef6bf58165012e611ff594f1e0c0e4
marcin.pierchala@icloud.com <marcin.pierchala@icloud.com>
2023-09-21 18:45:25 +0200

    extendRegisterProps

--- PhotosForSale/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ PhotosForSale/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -108,6 +108,12 @@ namespace PhotosForSale.Areas.Identity.Pages.Account
             public string? Role { get; set; }
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get; set; }
+
+            public string? StreetAddress { get; set; }
+            public string? City { get; set; }
+            public string? State { get; set; }
+            public string? PostalCode { get; set; }
+            public string? PhoneNumber { get; set; }
         }
 
 
