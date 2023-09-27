645f8fc7731bd61ec6bd66a1cc4937bf9fc2c2b6
MarcinPierchala <macintecmarcin@gmail.com>
2023-09-27 21:32:09 +0200

    CreateSummaryTemplate

--- PhotosForSale/Areas/Customer/Controllers/CartController.cs
+++ PhotosForSale/Areas/Customer/Controllers/CartController.cs
@@ -34,6 +34,11 @@ namespace PhotosForSale.Areas.Customer.Controllers
             return View(ShoppingCartVM);
         }
 
+        public IActionResult Summary() 
+        {
+            return View();
+        }
+
         public IActionResult Remove(int cartId) 
         {
             var cartFromDb = _unitOfWork.ShoppingCart.Get(u=>u.Id == cartId);
