be692dc0586d1bf9f9249a6aa7afe03639b7229a
MarcinPierchala <macintecmarcin@gmail.com>
2023-09-27 20:08:00 +0200

    AddCartVMAndShoppingCastIndexPage

---
+++ PhotosForSale/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Photos.DataAccess.Repository.IRepository;
+using Photos.Models.Models.ViewModels;
+using System.Security.Claims;
+
+namespace PhotosForSale.Areas.Customer.Controllers
+{
+    [Area("customer")]
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            ShoppingCartVM = new()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
+                includeProperties: "MyPhoto")
+            };
+            return View(ShoppingCartVM);
+        }
+    }
+}
