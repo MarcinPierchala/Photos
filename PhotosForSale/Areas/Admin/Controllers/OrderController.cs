20371ec1e8ef69950f9f588939c8a8d4510ded38
MarcinPierchala <macintecmarcin@gmail.com>
2024-02-26 20:26:42 +0100

    Add payment status filtering buttons

---
+++ PhotosForSale/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Photos.DataAccess.Repository.IRepository;
+using Photos.Models.Models;
+
+namespace PhotosForSale.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            return Json(new { data = objOrderHeaders });
+        }
+
+        #endregion
+    }
+}
