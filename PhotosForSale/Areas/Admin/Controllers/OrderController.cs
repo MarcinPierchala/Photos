0f28c87fc6520e4eaa171f050a6979d81d20b618
MarcinPierchala <macintecmarcin@gmail.com>
2024-03-02 20:00:19 +0100

    set summary bttns visibility during the situation

--- PhotosForSale/Areas/Admin/Controllers/OrderController.cs
+++ PhotosForSale/Areas/Admin/Controllers/OrderController.cs
@@ -6,6 +6,7 @@ using Photos.Models.Models;
 using Photos.Models.Models.ViewModels;
 using Photos.Utility;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace PhotosForSale.Areas.Admin.Controllers
 {
@@ -64,7 +65,19 @@ namespace PhotosForSale.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult GetAll(string status)
         {
-            IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            IEnumerable<OrderHeader> objOrderHeaders;
+
+            if(User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
+            {
+                objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            }
+            else
+            {
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+                objOrderHeaders = _unitOfWork.OrderHeader
+                    .GetAll(u => u.ApplicationUserId == userId, includeProperties: "ApplicationUser").ToList();
+            }
             switch (status)
             {
                 case "pending":
