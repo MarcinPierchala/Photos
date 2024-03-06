23110ecc75e8c8908849fd82b6a21e932dc34fde
MarcinPierchala <macintecmarcin@gmail.com>
2024-03-06 18:14:17 +0100

    add creeating user from the admin side

--- PhotosForSale/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ PhotosForSale/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -208,7 +208,14 @@ namespace PhotosForSale.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if(User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
+                        {
+                            TempData["success"] = "New user created successfully";
+                        }
+                        else
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);                            
+                        }
                         return LocalRedirect(returnUrl);
                     }
                 }
