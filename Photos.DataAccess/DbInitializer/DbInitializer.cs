a704a8b5b7aed9e88873a17347b56e04353b024d
MarcinPierchala <macintecmarcin@gmail.com>
2024-03-06 19:05:44 +0100

    add DbInitializer automatic Admin Createing Method

---
+++ Photos.DataAccess/DbInitializer/DbInitializer.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Photos.DataAccess.Data;
+using Photos.Models.Models;
+using Photos.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Photos.DataAccess.DbInitializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbConext _db;
+
+        public DbInitializer(
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            ApplicationDbConext db)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _db = db;
+        }
+        public void Initialize()
+        {
+            //migrations if not applied
+            try
+            {
+                if(_db.Database.GetPendingMigrations().Count() > 0) 
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch(Exception ex) { }
+
+            //roles if not exists
+            if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
+
+                //admin superuser create 
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "macintecmarcin@gmail.com",
+                    Email = "macintecmarcin@gmail.com",
+                    Name = "Marcin Super Admin",
+                    StreetAddress = "Main Street 0001",
+                    City = "Glasgow",
+                    PostalCode = "x6AC21B",
+                    State = "Silesia",
+                    PhoneNumber = "2956 A3CD"
+                }, "Admin123*").GetAwaiter().GetResult();
+
+                ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "macintecmarcin@gmail.com");
+                _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+            }
+            return;
+        }
+    }
+}
